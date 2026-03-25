6304d4a18cb8b1af807e3f83115b969e5c716c18
RichardDorian <richard.dorian@outlook.com>
2026-03-25 10:26:39 +0100

    chore(db): add city table

--- database.sql
+++ database.sql
@@ -1,4 +1,9 @@
 CREATE TABLE city (
   id UNSIGNED INT PRIMARY KEY,
-  department_code 
-)
+  department_code VARCHAR NOT NULL,
+  insee_code VARCHAR,
+  zip_code VARCHAR,
+  name VARCHAR NOT NULL,
+  lat FLOAT NOT NULL,
+  lon FLOAT NOT NULL
+);
