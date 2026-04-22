b684b236259cb9ab9f279ce3c3461ea7488e6ded
RichardDorian <richard.dorian@outlook.com>
2026-04-22 13:43:54 +0200

    chore(db): setup initial migration

--- database.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE city (
-  id UNSIGNED INT PRIMARY KEY,
-  department_code VARCHAR NOT NULL,
-  insee_code VARCHAR,
-  zip_code VARCHAR,
-  name VARCHAR NOT NULL,
-  lat FLOAT NOT NULL,
-  lon FLOAT NOT NULL
-);
